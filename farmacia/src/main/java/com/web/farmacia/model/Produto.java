12462710692b9b3d4ef00b3526958e83e4fb6558
Vitor Melo <vitormelo9913@gmail.com>
2022-02-24 11:09:28 -0300

    @vitor-pm [Add Indentity]

--- farmacia/src/main/java/com/web/farmacia/model/Produto.java
+++ farmacia/src/main/java/com/web/farmacia/model/Produto.java
@@ -1,6 +1,8 @@
 package com.web.farmacia.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -10,6 +12,7 @@ import javax.validation.constraints.NotNull;
 public class Produto {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotNull
