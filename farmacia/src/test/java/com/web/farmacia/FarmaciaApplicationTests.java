8b06b9209304652d3510709f73ee5cddb7305340
RiqueMendes <97553942+RiqueMendes@users.noreply.github.com>
2022-02-24 10:53:20 -0300

    @RiqueMendes commit

---
+++ farmacia/src/test/java/com/web/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
