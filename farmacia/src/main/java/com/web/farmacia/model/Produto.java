d9e449e502d03b3eac233929fb63d008fc76d646
Vitor Melo <vitormelo9913@gmail.com>
2022-02-24 11:07:26 -0300

    @vitor-pm [Add PrudutoModel]

---
+++ farmacia/src/main/java/com/web/farmacia/model/Produto.java
@@ -0,0 +1,45 @@
+package com.web.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    private int id;
+
+    @NotNull
+    private String titulo;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @NotNull
+    private String descricao;
+
+}
